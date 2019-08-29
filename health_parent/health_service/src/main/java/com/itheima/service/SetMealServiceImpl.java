7c5a3a4dc4c6502ec88551140f277bf687cd4c7c
Lihaiwen <1037983528@qq.com>
2019-08-29 20:36:12 +0800

    李海文 5 将数据存放到Redis中  2019-08-29

--- health_parent/health_service/src/main/java/com/itheima/service/SetMealServiceImpl.java
+++ health_parent/health_service/src/main/java/com/itheima/service/SetMealServiceImpl.java
@@ -1,13 +1,17 @@
 package com.itheima.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
-//import com.itheima.constant.RedisConstant;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.itheima.dao.SetMealDao;
 import com.itheima.pojo.CheckGroup;
 import com.itheima.pojo.Setmeal;
 import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -43,19 +47,47 @@ public class SetMealServiceImpl implements SetMealService {
     }
 
     @Override
-    public List<Setmeal> getSetmealList() {
-        List<Setmeal> list = setMealDao.getSetmealList();
-        return list;
+    public List<Setmeal> getSetmealList() throws IOException {
+        //LiHaiWen  在Redis中获得所有套餐信息json字符串   2019-08-28
+        Jedis jedis = jedisPool.getResource();
+        String allSetmealJsonStr = jedis.get("AllSetmeal");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        if(allSetmealJsonStr==null){
+            //如果第一次查询，从数据库中查询数据存放到Redis中
+            List<Setmeal> list = setMealDao.getSetmealList();
+            allSetmealJsonStr = objectMapper.writeValueAsString(list);
+            jedis.set("AllSetmeal",allSetmealJsonStr);
+            System.err.println("第一次访问存放到Redis");
+        }
+        //将json数据转换为list集合
+        List<Setmeal> SetmealList = objectMapper.readValue(allSetmealJsonStr,new TypeReference<List<Setmeal>>(){});
+        //打印验证
+        System.err.println("SetmealList = " + SetmealList);
+        return SetmealList;
     }
 
     @Override
-    public Setmeal findById(Integer id, Integer type) {
-        Setmeal setmeal = setMealDao.findById(id);
-        if (type==1){
-            List<CheckGroup> checkGroups = setmeal.getCheckGroups();
-            System.out.println("checkGroups = " + checkGroups.size());
+    public Setmeal findById(Integer id, Integer type) throws IOException {
+
+        //LiHaiWen  在Redis中获得当前id套餐信息json字符串   2019-08-28
+        Jedis jedis = jedisPool.getResource();
+        String setmealJsonStr = jedis.get("Setmeal"+id);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        if(setmealJsonStr==null){
+            //如果第一次查询，从数据库中查询数据存放到Redis中
+            Setmeal setmeal = setMealDao.findById(id);
+            setmealJsonStr = objectMapper.writeValueAsString(setmeal);
+            jedis.set("Setmeal"+id,setmealJsonStr);
+            System.err.println("第一次访问存放到Redis");
         }
-        System.out.println(setmeal);
+        //将json数据转换为对象
+        Setmeal setmeal = objectMapper.readValue(setmealJsonStr, new TypeReference<Setmeal>(){});
+        //打印验证
+        System.err.println("setmeal = " + setmeal);
         return setmeal;
     }
 
